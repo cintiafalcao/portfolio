79e787278bfc3d80feef518c53ba7810965a5382
Cintia de Carvalho Falcão <cintia@Cintias-MacBook-Pro.local>
2017-07-21 08:28:07 +0200

    buzzfeat bug

--- src/projects/Buzzfeat.js
+++ src/projects/Buzzfeat.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Image, Project, Title, Text } from '../Case.js'
+import { Image, Project, Title, Text, ImageGroup } from '../Case.js'
 import cover from '../images/buzzfeat/cover.jpg'
 import img_1 from '../images/buzzfeat/f1.jpg'
 import img_2 from '../images/buzzfeat/f2.jpg'
@@ -19,23 +19,29 @@ export default props =>
     <Image src={cover} alt='Buzzfeat' />
     <Text>Buzzfeed was a web platform for development that allowed users to create their own fully customized web sites and also offer a series of pre-customized templates</Text>
     <Title>Design templates</Title>
-    <Image src={img_1} alt='Buzzfeat' /><br />
+    <ImageGroup>
+      <div><Image src={img_1} alt='Buzzfeat' /></div>
+      <div><Image src={img_2} alt='Buzzfeat' /></div>
+    </ImageGroup>
+    <Title>Fashion templates</Title>
+    <ImageGroup>
+      <div><Image src={img_3} alt='Buzzfeat' /></div>
+      <div><Image src={img_4} alt='Buzzfeat' /></div>
+    </ImageGroup>
 
-    <Image src={img_2} alt='Buzzfeat' /><br />
+    <Image src={img_5} alt='Buzzfeat' />
+    <Title>Fitness templates</Title>
+    <ImageGroup>
+      <div><Image src={img_6} alt='Buzzfeat' /></div>
+      <div><Image src={img_7} alt='Buzzfeat' /></div>
+    </ImageGroup>
+    <Title>Library templates</Title>
+    <ImageGroup>
+      <div><Image src={img_8} alt='Buzzfeat' /></div>
+      <div><Image src={img_9} alt='Buzzfeat' /></div>
+    </ImageGroup>
 
-    <Image src={img_3} alt='Buzzfeat' /><br />
- 
-    <Image src={img_4} alt='Buzzfeat' /><br />
-
-    <Image src={img_5} alt='Buzzfeat' /><br />
-
-    <Image src={img_6} alt='Buzzfeat' /><br />
-
-    <Image src={img_7} alt='Buzzfeat' /><br />
-  
-    <Image src={img_8} alt='Buzzfeat' /><br />
-   
-    <Image src={img_9} alt='Buzzfeat' /><br />
-
+   <Title>Store templates</Title>
     <Image src={img_10} alt='Buzzfeat' />
+
   </Project>
